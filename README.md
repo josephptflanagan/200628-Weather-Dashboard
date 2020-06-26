c223f78eaebe70034f01b42be438ed94b78d9b2a
Joe Flanagan <jflanagan@nieceequipment.local>
2020-06-26 13:23:59 -0500

    added search bar, access to API

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # 200628 Weather Dashboard
 
 ## GitHub Repository URL
+https://github.com/josephptflanagan/200628-Weather-Dashboard
 
 ## Site URL
+https://josephptflanagan.github.io/200628-Weather-Dashboard/
 
 ## Screenshot
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,45 @@
+function getWeatherData(cityName){
+
+    //create the address to access the api for the chosen city
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=258563bcd408b087604452eb2e20b86f"
+
+    fetch(weatherApiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayWeatherData(data, cityName);
+            })
+        }
+        else{
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error){
+        alert("Unable to Access Open Weather");
+    })
+};
+
+function displayWeatherData(data, cityName){
+    console.log(data);
+
+    var cityButton = $("<button>")
+        .addClass("btn")
+        .text(cityName);
+
+    $("#locations").append(cityButton);
+};
+
+//when the search bar is 
 $("#search-button").on("click", function(){
-    var searchTerm = $(this).parent().siblings("#search-bar").text();
+
+    //get and store the city name from the search bar
+    var cityName = $(this).siblings("#search-bar").val().trim();
     
-})
+    if(cityName == ""){
+        return;
+    }
+    else{
+    //Call the function that grabs the data
+    getWeatherData(cityName);
+    }
+    
+});
